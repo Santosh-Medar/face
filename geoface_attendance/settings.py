bb9c31bd145838fbbcd054dc98c0f70b423423e0
Santosh Medar <medarsantosh04@gmail.com>
2026-06-11 08:57:28 +0530

    Fix WhiteNoise for admin static files

--- geoface_attendance/settings.py
+++ geoface_attendance/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -63,6 +64,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ROOT_URLCONF = 'geoface_attendance.urls'
 
 TEMPLATES = [
